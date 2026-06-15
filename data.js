a88b65a3d1825a4843d655133aec8bb678983707
chiroru-taketani <x22067xx@aitech.ac.jp>
2026-06-15 13:00:30 +0900

    内容変更

--- data.js
+++ data.js
@@ -123,6 +123,9 @@ const portfolioData = [
             ],
             architecture: [
                 "会場に設置したマイクに入力された音声をリアルタイムに取得し、音量に応じて映像に花火を打ち上げるシステムを開発。プロトタイプ完成の速さやGLSL環境からTouchDesignerを使用して開発"
+                { images: [
+                    "assets/works/02/shystem.png",
+                ] },
             ],
             
         
